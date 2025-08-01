539fa4eac98a70cfb2812a6cfd850c497dd7c710
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-08-01 10:38:21 +0500

    Set up server in server.js using express-js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Test
+const express = require("express");
+const path = require("path");
+const app = express();
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(session({
+        cookie: { maxAge: 86400000 },
+        store: new MemoryStore({
+            checkPeriod: 86400000
+        }),
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    })
+);
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
