3c879f8a5b1dee020e35687c78e0f0a1a9301594
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-08-04 11:41:00 +0500

    Created awareness section

---
+++ public/script/home.js
@@ -0,0 +1,15 @@
+const heroRadioButtons = document.querySelectorAll(".radio-button");
+
+function radioButtonClick(btn) {
+    const buttons = document.querySelectorAll(".radio-button");
+    buttons.forEach((e) => {
+        e.classList.remove("radio-clicked");
+    });
+    btn.classList.add("radio-clicked");
+}
+
+heroRadioButtons.forEach((e) => {
+    e.addEventListener("click", () => {
+        radioButtonClick(e);
+    });
+});
