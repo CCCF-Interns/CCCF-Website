c1b90fc8a8aa124b8655e0cf3b11529f81248a5e
Imranullah Khan <imranullahkhann2004@gmail.com>
2025-08-05 13:37:39 +0500

    Change in style standards

--- utils/server.js
+++ utils/server.js
@@ -1,15 +1,15 @@
-import path from 'path';
-import express from 'express';
-import memorystore from 'memorystore';
-import session from 'express-session';
-import viewsRoutes from '../routes/viewsRoutes.js';
-import __dirname from '../dirname.js';
+import path from "path";
+import express from "express";
+import memorystore from "memorystore";
+import session from "express-session";
+import viewsRoutes from "../routes/viewsRoutes.js";
+import __dirname from "../dirname.js";
 
 function createServer () {
     const app = express();
     const MemoryStore = memorystore(session);
 
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, "public")));
     app.use(express.json());
     app.use(session({
             cookie: { maxAge: 86400000 },
@@ -27,7 +27,7 @@ function createServer () {
 
     app.use((err, req, res, next) => {
         console.error(err.stack);
-        res.status(500).send('Something broke!');
+        res.status(500).send("Something broke!");
     });
 
     return app;
