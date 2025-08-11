0a5e1a1f19833ec173e871b79e17587affbb2563
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-08-11 12:41:04 +0500

    Created gallery.js

---
+++ public/script/gallery.js
@@ -0,0 +1,13 @@
+// imageData = {
+//     id: 2,
+//     title: 'Yeah',
+//     image_url: "Kashif"
+// };
+
+// fetch('/api/gallery/insert', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(imageData),
+// });
