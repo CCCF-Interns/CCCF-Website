dd9e08e0e83009281c92c0eccd22b2eddfa317ec
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-08-18 19:12:15 +0500

    Can view images now

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -1,8 +1,15 @@
 const imagesContainer = document.querySelector("#images-container");
 const loader = document.querySelector("#loader");
-let images;
+const imageOverlay = document.querySelector("#image-overlay");
+const imagePreview = document.querySelector("#image-preview");
+const previewLeft = document.querySelector("#preview-left");
+const previewRight = document.querySelector("#preview-right");
+const previewClose = document.querySelector("#preview-close");
 
-function createImage(title, link) {
+let images;
+let currentImageIndex;
+
+function createImage(title, link, index) {
     const container = document.createElement("div");
     const image = document.createElement("img");
 
@@ -11,6 +18,13 @@ function createImage(title, link) {
 
     image.src = link;
 
+    container.addEventListener("click", () => {
+        imagePreview.src = link;
+        imageOverlay.style.display = 'block';
+        document.body.style.overflow = "hidden";
+        currentImageIndex = index;
+    });
+
     container.appendChild(image);
     imagesContainer.appendChild(container);
 }
@@ -18,13 +32,52 @@ function createImage(title, link) {
 async function createImages() {
     const response = await fetch ("/api/gallery");
     images = await response.json();
+    let counter = 0;
 
     for (let x of images.data) {
-        createImage(x.title, x.image_url);
+        createImage(x.title, x.image_url, counter);
+        ++counter;
     }
 
     loader.style.display = "none";
     document.body.style.overflow = "auto";
 }
+
+previewLeft.addEventListener("click", () => {
+    if (!images)
+        return;
+
+    if (currentImageIndex == 0) {
+        currentImageIndex = images.data.length - 1;
+    }
+    else {
+        currentImageIndex--;
+    }
+
+    imagePreview.src = images.data[currentImageIndex].image_url;
+    imageOverlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+});
+
+previewRight.addEventListener("click", () => {
+    if (!images)
+        return;
+
+    if (currentImageIndex == images.data.length - 1) {
+        currentImageIndex = 0;
+    }
+    else {
+        currentImageIndex++;
+    }
+
+    imagePreview.src = images.data[currentImageIndex].image_url;
+    imageOverlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+});
+
+previewClose.addEventListener("click", () => {
+    imageOverlay.style.display = "none";
+    document.body.style.overflow = "auto";
+});
 
 createImages();
