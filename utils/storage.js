5ef2119595003b950ae92637f8981ec84d500779
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-08-19 14:27:50 +0500

    removed readR2

--- utils/storage.js
+++ utils/storage.js
@@ -1,13 +1,4 @@
-import AWS from "aws-sdk";
 import { writeR2 } from "../app.js";
-
-const readR2 = new AWS.S3({
-    endpoint: `https://${process.env.R2_ID}.r2.cloudflarestorage.com`,
-    accessKeyId: process.env.R2_READ_KEY,
-    secretAccessKey: process.env.R2_READ_SECRET,
-    signatureVersion: 'v4',
-    region: 'auto',
-});
 
 async function uploadFile(key, file) {
     await writeR2.putObject({
@@ -17,13 +8,5 @@ async function uploadFile(key, file) {
         ContentType: file.mimetype,
     }).promise();
 }
-
-// async function readFile(key) {
-//     const res = await readR2.getObject({
-//         Bucket: process.env.R2_BUCKET,
-//         Key: key,
-//     }).promise();
-//     return res.Body.json();
-// }
 
 export default uploadFile;
