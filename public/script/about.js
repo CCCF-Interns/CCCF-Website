bafd54ce4569639756d657c660ba728f6dda2661
Faiq Abdullah <faiqabdullah2b@gmail.com>
2025-10-06 13:48:31 +0500

    i know this is the contact us branch lekin about us ka bhi kaam karliya

--- public/script/about.js
+++ public/script/about.js
@@ -9,7 +9,7 @@ async function loadData() {
     membersData = result.data;
 }
 
-function addEmployee(name, title, description, imageSource) {
+function addEmployee(name, title, description, mediaType, mediaURL, imageSource) {
 
     imageSource = imageSource || "/assets/images/dummyProfile.png";
 
@@ -21,6 +21,7 @@ function addEmployee(name, title, description, imageSource) {
     let memberName = document.createElement("div");
     let memberTitle = document.createElement("div");
     let memberDescription = document.createElement("div");
+    let memberMedia = document.createElement("div");
 
     memberImage.classList.add("member-image");
     memberImage.src = imageSource;
@@ -38,13 +39,50 @@ function addEmployee(name, title, description, imageSource) {
     memberDescription.textContent = description;
     newMember.appendChild(memberDescription);
 
+    memberMedia.classList.add("member-socials");
+
+    for (let i = 0; i < mediaType.length; i++) {
+        let mediaAnchor = document.createElement("a");
+        mediaAnchor.target = "_blank";
+        let mediaImg = document.createElement("img");
+        console.log(mediaType);
+
+        switch(mediaType[i]) {
+            case 'I':
+                mediaImg.src = "/assets/svg/Instagram.svg";
+                mediaImg.alt = "Instagram";
+                mediaAnchor.href = mediaURL[i];
+                console.log(mediaURL[i])
+                break;
+            case 'X':
+                mediaImg.src = "/assets/svg/X.svg";
+                mediaImg.alt = "X";
+                mediaAnchor.href = mediaURL[i];
+                break;
+            case 'L':
+                mediaImg.src = "/assets/svg/Linkedin.svg";
+                mediaImg.alt = "Linkedin";
+                mediaAnchor.href = mediaURL[i];
+                break;
+            case 'F':
+                mediaImg.src = "/assets/svg/Facebook.svg";
+                mediaImg.alt = "Facebook";
+                mediaAnchor.href = mediaURL[i];
+                break;
+        }
+        mediaAnchor.appendChild(mediaImg);
+        memberMedia.appendChild(mediaAnchor);
+    }
+
+    newMember.appendChild(memberMedia);
+
     teamMembers.appendChild(newMember);
 }
 
 async function initializeMembers() {
     await loadData();
     for (let x of membersData) {
-        addEmployee(x.name, x.job_title, x.description, x.image_url);
+        addEmployee(x.name, x.job_title, x.description, [], [], x.imageURL);
     }
     document.body.style.overflow = "auto";
     loader.style.display = "none";
